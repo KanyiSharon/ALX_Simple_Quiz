6b29dcabd2ef81d56dfa59cc72c298541ec8d484
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-07-17 02:19:47 +0300

    made changes to quiz.js

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer() {
   var correctAnswer = "4";
   var userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
   var feedback = document.getElementById("feedback");
-  if (userAnswer == correctAnswer) {
+  if (userAnswer === correctAnswer) {
     feedback.textContent = "Correct! Well done.";
   } else {
     feedback.textContent = "That's incorrect. Try again!";
