c031576991a2e62904f52dc6c10d02f6e69e6657
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-07-17 02:17:43 +0300

    made changes to quiz.js

--- quiz.js
+++ quiz.js
@@ -1,12 +1,12 @@
-function checkAnswer(){
-  var correctAnswer="4";
-  var userAnswer=document.querySelector('input[name="quiz"]:checked')?.value;
-  if(userAnswer==correctAnswer){
-    var feedback=document.querySelector('#feedback');
+function checkAnswer() {
+  var correctAnswer = "4";
+  var userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
+  var feedback = document.getElementById("feedback");
+  if (userAnswer == correctAnswer) {
     feedback.textContent = "Correct! Well done.";
-}else{
-  feedback.textContent ="That's incorrect. Try again!";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }
 }
-}
-var submitButton=document.querySelector('#submit');
-submitButton.addEventListener('click',checkAnswer)
+var submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", checkAnswer);
